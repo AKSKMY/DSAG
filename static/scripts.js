d61921e44804304c12552354037c95c6ced999d7
NEO ZHIYONG <w0shiizhiyong@gmail.com>
2024-05-29 00:03:18 +0800

    Update
    Added the drawing of warehouse navigation based on the nodes
    Added function to calculate distance between each nodes
    Added example of dijkstra algorithm that is going to be implemented

---
+++ static/scripts.js
@@ -0,0 +1,19 @@
+
+// // Function to initialize the map and display traffic layer
+// function initMap() {
+//     if (typeof google === 'undefined' || typeof google.maps === 'undefined') {        
+//         // Google Maps API not loaded, handle error or retry loading
+//         console.error('Google Maps API not loaded.');        
+//         return;
+//     }
+//     const map = new google.maps.Map(document.getElementById("map"), {        
+//         zoom: 12,
+//         center: { lat: 1.3644, lng: 103.9915 } // Centered at Changi Airport T3    
+//     });
+//     window.map = map;
+
+// }
+
+// window.onload = function() {    
+//     initMap();
+// };
