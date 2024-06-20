515eebd6c5f7c263efb299c446a6c0bfb230b1f8
rachelletwq <2302259@sit.singaporetech.edu.sg>
2024-06-20 23:38:03 +0800

    ABC update (incomplete)

--- ABC.py
+++ ABC.py
@@ -4,43 +4,48 @@ from abc_analysis import abc_analysis, abc_plot
 
 
 # Load the datasets
-file_path = "Dataset\Retail_Transactions_Dataset.csv"
-data = pd.read_csv(file_path)
+#file_path = "Dataset\Retail_Transactions_Dataset.csv"
+#data = pd.read_csv(file_path)
 
-file_path = "Dataset\item_count.csv"
+file_path = "Dataset\\item_count.csv"
 item = pd.read_csv(file_path)
 
 
 # Get the number of total sales for each product
-def countItems():
+#def countItems():
     # Split by quotation marks and count the occurrences of each item
     # Will count , [ and ] symbols as well
-    itemCount = data['Product'].str.split('\'').explode().value_counts()
-    print(itemCount)
+    #itemCount = data['Product'].str.split('\'').explode().value_counts()
+    #print(itemCount)
 
     # Write wordCounts to a CSV file
-    itemCount.to_csv('itemCount.csv')
+    #itemCount.to_csv('itemCount.csv')
 
 
 def ABC():
-    item['AddCost'] = item['Price'] * item['Count']
+    global item
+    item['Total_Cost'] = item['Price'] * item['Count']
+    # sort in descending 
+    item = item.sort_values(by='Total_Cost', ascending = False)
+    #calculate percentage
+    item['Item_percent'] = (item['Total_Cost'] / item['Total_Cost'].sum()) * 100
     # Plot the ABC analysis
-    abc = abc_analysis(item['AddCost'], True)
+    #abc = abc_analysis(item['AddCost'], True)
 
     # idnex position of A, B, and C Segmentations
-    a_index = abc['Aind']
-    b_index = abc['Bind']
-    c_index = abc['Cind']
+    #a_index = abc['Aind']
+    #b_index = abc['Bind']
+    #c_index = abc['Cind']
 
     # New Column indicating A, B, or C
-    cond_list = [item.index.isin(a_index),
-                item.index.isin(b_index),
-                item.index.isin(c_index)]
+    #cond_list = [item.index.isin(a_index),
+                #item.index.isin(b_index),
+                #item.index.isin(c_index)]
 
-    choice_list = ['A','B','C']
+    #choice_list = ['A','B','C']
 
-    item['abc'] = np.select(cond_list, choice_list)
-    item.sort_values(by=['AddCost'], ascending=False)
+    #item['abc'] = np.select(cond_list, choice_list)
+    #item.sort_values(by=['AddCost'], ascending=False)
 
     print(item.head(20))
 
