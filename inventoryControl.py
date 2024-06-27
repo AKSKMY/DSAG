7bc886351b1a86ab7e507bc58cbef443bbce82cd
zhiyi-laptop <>
2024-06-27 09:51:42 +0800

    Update EOQ and JIT
    Using Linear Regression for demand forecast. EOQ changed from inbuilt function to manual calculation

--- inventoryControl.py
+++ inventoryControl.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from stockpyl.eoq import economic_order_quantity
 
 file_path = "Dataset\Retail_Transactions_Dataset.csv"
 data = pd.read_csv(file_path)
@@ -13,7 +13,7 @@ data = pd.read_csv(file_path)
 file_path = "Dataset\item_count.csv"
 dataSales = pd.read_csv(file_path)
 
-def filterData():
+def splitDate():
     # Convert the 'Date' column to datetime format
     data['Date'] = pd.to_datetime(data['Date'])
 
@@ -129,25 +129,41 @@ def JIT(data, demand_forecast):
 # Economic Order Quantity (EOQ) calculation
 # Find out the optimal order quantity that minimizes the total inventory costs
 def EOQ():
-    eoqValues = []
-    costValues = []
-    for index, row in dataSales.iterrows():
-        price = row['Price']
-        count = row['Count']
-        
+    # Initialize a dataframe to store the EOQ values
+    EOQ_df = pd.DataFrame()
+
+    # Iterate over each unique product
+    for product in dataSales['Product'].unique():
+        product_data = historicSales(data, product)
+
+        demand_rate = product_data[-12:]['Sales'].sum()
+        fixed_cost = dataSales[dataSales['Product'] == product]['Price'].values[0]
+        holding_cost = fixed_cost * 0.1
+
         # Holding cose is 10% of the price for now. Change?
-        Q, cost = economic_order_quantity(fixed_cost=price, holding_cost=(price * 0.1), demand_rate=count)
-        eoqValues.append(Q)
-        costValues.append(cost)
+        EOQ = math.sqrt((2 * demand_rate * fixed_cost) / holding_cost)
+        total_cost = (demand_rate * fixed_cost / EOQ) + (EOQ / 2 * holding_cost)
 
-    dataSales['EOQ'] = eoqValues
-    dataSales['OrderCost'] = costValues
-    print(dataSales.head(20))
+        # Append the results to the DataFrame
+        # Create a new_row with the product and EOQ values
+        new_row = {
+            'Product': product,
+            'EOQ': round(EOQ),
+            'TotalOrderCost': round(total_cost, 2)
+        }
+
+        # Convert new_row to a DataFrame
+        new_row_df = pd.DataFrame([new_row])
+
+        # Adding new_row_df to prediction_df
+        EOQ_df = pd.concat([EOQ_df, new_row_df], ignore_index=True, axis=0)
+
+    return EOQ_df
 
 
-
-filterData()
+splitDate()
+# print(historicSales(data, "Jam"))
+# print(linearRegression(data))
 # JIT(data, linearRegression(data))
-print(linearRegression(data))
 
-# EOQ()
+print(EOQ())
